bc969c3a89f359ba0086d07441691a4d76770d39
TeaWoo0219 <gmrgud0317@gmail.com>
2024-09-30 20:34:00 +0900

    Update farm_sensor.py

--- farm_sensor.py
+++ farm_sensor.py
@@ -12,9 +12,6 @@ firebase_admin.initialize_app(cred, {
     'databaseURL': 'https://weather-6a3c7-default-rtdb.firebaseio.com/'  # Firebase Realtime Database URL
 })
 
-# DHT22 센서 설정
-DHT_SENSOR = adafruit_dht.DHT22(board.D2)  # DHT22 센서를 사용하고 GPIO 2 (BCM) 핀에 연결
-
 # GPIO 설정
 GPIO.setmode(GPIO.BCM)
 
@@ -23,29 +20,23 @@ ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
 time.sleep(2)  # 포트 안정화 대기
 
 # Firebase에 데이터를 업로드하는 함수
-def upload_to_firebase(CO2, Temperature, humidity, illuminance, phVal, waterTemp):
-    try:
-        # 날짜별로 경로 설정
-        date_path = time.strftime('%Y-%m-%d')  # 예: "2023-08-21"
-        time_path = time.strftime('%H-%M-%S')
-        ref = db.reference(f'sensorData/{date_path}/{time_path}')
+def upload_to_firebase(co2, temperature, humidity, lux, ph_val, water_temp):
+    # 날짜별로 경로 설정
+    date_path = time.strftime('%Y-%m-%d')  # 예: "2023-08-21"
+    time_path = time.strftime('%H-%M-%S')
+    ref = db.reference(f'sensorData/{date_path}/{time_path}')
 
-        # 데이터 업로드
-        ref.push({
-            'CO2': CO2,
-            'Temperature': Temperature,
-            'humidity': humidity,
-            'lux': illuminance,
-            'pH': phVal,
-            'water_temperature': waterTemp,
-            'timestamp': time.strftime('%Y-%m-%d %H:%M:%S')
-        })
-        print("Data uploaded to Firebase")
-    except Exception as e:
-        print(f"Failed to upload data to Firebase: {e}")
-
-# 데이터를 저장할 전역 변수 초기화
-CO2, Temperature, humidity, illuminance, phVal, waterTemp = None, None, None, None, None, None
+    # 데이터 업로드
+    ref.push({
+        'co2': co2,
+        'temperature': temperature,
+        'humidity': humidity,
+        'lux': lux,
+        'pH': ph_val,
+        'water_temperature': water_temp,
+        'timestamp': time.strftime('%Y-%m-%d %H:%M:%S')
+    })
+    print("Data uploaded to Firebase")
 
 try:
     while True:
@@ -56,40 +47,30 @@ try:
 
             # 데이터 파싱
             try:
-                if "CO2" in receivedData and "Temperature" in receivedData:
-                    # CO2와 Temperature, Humidity 값 함께 추출
-                    data_parts = receivedData.split(", ")
-                    
-                    # CO2 값 추출 (음수 처리 가능)
-                    CO2 = float(data_parts[0].split(": ")[1].replace(" ppm", ""))
-                    
-                    # 공기 온도 값 추출
-                    Temperature = float(data_parts[1].split(": ")[1].replace("C", ""))
-                    
-                    # 습도 값 추출
-                    humidity = float(data_parts[2].split(": ")[1].replace("%", ""))
+                co2_part, temp_part, hum_part, lux_part, ph_part, water_temp_part = receivedData.split(", ")
+                
+                # CO2 값 추출
+                co2_value = int(co2_part.split(": ")[1])
+                
+                # 온도 값 추출
+                temp_value = float(temp_part.split(": ")[1])
+                
+                # 습도 값 추출
+                hum_value = float(hum_part.split(": ")[1])
+                
+                # 조도 값 추출
+                lux_value = float(lux_part.split(": ")[1])
+                
+                # pH 값 추출
+                ph_val = float(ph_part.split(": ")[1])
+                
+                # 수온 값 추출
+                water_temp_value = float(water_temp_part.split(": ")[1])
 
-                elif "Illuminance" in receivedData:
-                    # 조도 값 추출
-                    illuminance = float(receivedData.split(": ")[1].replace(" lx", ""))
-
-                elif "pH Value" in receivedData:
-                    # pH 값 추출
-                    phVal = float(receivedData.split(": ")[1])
-
-                elif "Water Temperature" in receivedData:
-                    # 수온 값 추출
-                    waterTemp = float(receivedData.split(": ")[1].replace(" C", ""))
-
+                # Firebase에 데이터 업로드
+                upload_to_firebase(co2_value, temp_value, hum_value, lux_value, ph_val, water_temp_value)
             except Exception as e:
                 print(f"Error parsing data: {e}")
-                continue  # 파싱 오류 발생 시 다음 루프로 넘어감
-
-            # 모든 데이터가 수신되었을 때만 Firebase에 업로드
-            if None not in [CO2, Temperature, humidity, illuminance, phVal, waterTemp]:
-                upload_to_firebase(CO2, Temperature, humidity, illuminance, phVal, waterTemp)
-                # Firebase에 데이터가 업로드되면 변수 초기화
-                CO2, Temperature, humidity, illuminance, phVal, waterTemp = None, None, None, None, None, None
 
         time.sleep(2)  # 2초마다 데이터 전송
 
